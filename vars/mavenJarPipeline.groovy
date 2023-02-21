482c11a85f2f23ccd33697212cecf3653ded5c10
MD MOQADDAS <sgn9556@gmail.com>
2023-02-21 23:41:30 +0530

    Updated the push stage

--- vars/mavenJarPipeline.groovy
+++ vars/mavenJarPipeline.groovy
@@ -13,8 +13,10 @@ def call(Map config){
 
          stage("Push"){
             sh 'docker build -t moqaddas/mavanapplication:$EXECUTOR_NUMBER .'
-            sh 'docker login https://docker.io -u $dockerUser -p $dockerPassword'
-            sh 'docker push moqaddas/mavanapplication:$EXECUTOR_NUMBER'
+            withCredentials([usernamePassword(credentialsId: '4636fbc0-97d9-4b53-a309-7121c3d91395', passwordVariable: 'pass', usernameVariable: 'user')]) 
+           {
+             sh "docker login -u ${user} -p ${pass}  https://docker.io"
+            }
         }
     }
 }
--- vars/reactJsPipeline.groovy
+++ vars/reactJsPipeline.groovy
@@ -20,9 +20,10 @@ def call(Map config){
          stage("Push"){
             sh 'docker build -t moqaddas/reactapplication:$EXECUTOR_NUMBER .'
            
-           withCredentials([usernamePassword(credentialsId: '4636fbc0-97d9-4b53-a309-7121c3d91395', passwordVariable: 'pass', usernameVariable: 'user')]) {
-    sh "docker login -u ${user} -p ${pass}  https://docker.io"
-}
+           withCredentials([usernamePassword(credentialsId: '4636fbc0-97d9-4b53-a309-7121c3d91395', passwordVariable: 'pass', usernameVariable: 'user')]) 
+           {
+             sh "docker login -u ${user} -p ${pass}  https://docker.io"
+            }
 
           
 
