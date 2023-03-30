2b22ebe0004c39f796163d45a6f45daf3156d925
MD MOQADDAS <sgn9556@gmail.com>
2023-03-30 17:42:17 +0530

    updated the pipeline

--- vars/reactJsPipeline.groovy
+++ vars/reactJsPipeline.groovy
@@ -19,7 +19,7 @@ def call(Map config){
          stage("Delivery"){
            withCredentials([usernamePassword(credentialsId: '4636fbc0-97d9-4b53-a309-7121c3d91395', passwordVariable: 'pass', usernameVariable: 'user')]) 
            {
-             sh "docker login -u ${user} -p ${pass}  https://docker.io"
+             sh "docker login -u ${user} -p ${pass} "
              
               sh "docker push moqaddas/reactapplication:$BUILD_NUMBER"
           sh "docker logout"
